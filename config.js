c7783b2bc01c3cfafbdbcc7068726c9ed1ec5dd9
sususurt <suruitong0428@hotmail.com>
2022-02-24 19:54:27 +0800

    CorsPassport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://127.0.0.1:27017/conFusion'
+    'mongoUrl' : 'mongodb://127.0.0.1:27017/conFusion',
+    'facebook': {
+        clientId: '1171610646979064',
+        clientSecret: '6c53d92cd912bc70717b60417c0496b6'
+    }
 }
